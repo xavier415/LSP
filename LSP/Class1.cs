db0f4c569a20e2bab26d2a8bcd731e61271b7740
xavier415 <xaviercolon061@gmail.com>
2024-08-02 20:42:04 -0400

    Add project files.

---
+++ LSP/Class1.cs
@@ -0,0 +1,59 @@
+﻿namespace LSP
+{
+    public abstract class Course
+    {
+        public int CourseId { get; set; }
+        public string Title { get; set; }
+
+        public abstract void Subscribe(Student std);
+        public abstract string GetCourseDetails();
+    }
+    public class OnlineCourse : Course
+    {
+        public override void Subscribe(Student std)
+        {
+            
+            Console.WriteLine($"El estudiante {std.Name} se ha suscrito al curso en línea {Title}.");
+        }
+
+        public override string GetCourseDetails()
+        {
+            return $"Curso en Línea: {Title}";
+        }
+    }
+
+    public class OfflineCourse : Course
+    {
+        public override void Subscribe(Student std)
+        {
+            
+            Console.WriteLine($"El estudiante {std.Name} se ha suscrito al curso presencial {Title}.");
+        }
+
+        public override string GetCourseDetails()
+        {
+            return $"Curso Presencial: {Title}";
+        }
+    }
+
+    public class HybridCourse : Course
+    {
+        public override void Subscribe(Student std)
+        {
+            
+            Console.WriteLine($"El estudiante {std.Name} se ha suscrito al curso híbrido {Title}.");
+        }
+
+        public override string GetCourseDetails()
+        {
+            return $"Curso Híbrido: {Title}";
+        }
+    }
+
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+    }
+
+}
